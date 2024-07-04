bf0658d289d420c800db852c73498806a29f85f6
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2024-07-04 13:37:35 +0530

    test for linear probing on large file

---
+++ golang/linear_probing_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"testing"
+)
+
+func TestCountsLinearProbing(t *testing.T) {
+	inbuiltMap := make(map[string]int, 8)
+	customMap := NewHashMapLinearProbing[string, uint](DefaultCapacity, DefaultLoadFactor)
+
+	f, err := os.Open("./tale.txt")
+	if err != nil {
+		panic("Unable to open file")
+	}
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanWords)
+
+	for scanner.Scan() {
+		w := scanner.Text()
+		// add to inbuilt map
+		_, ok := inbuiltMap[w]
+		if !ok {
+			inbuiltMap[w] = 0
+		}
+		inbuiltMap[w]++
+
+		// add to custom map
+		customMap.Put(w, customMap.GetOrDefault(w, 0) + 1)
+		customMap.clearNumCompares()
+	}
+	if customMap.n != uint(len(inbuiltMap)) {
+		t.Errorf("Expected length=%d, got=%d", len(inbuiltMap), customMap.n)
+	}
+	for w, expectedCount := range inbuiltMap {
+		actualCount, ok := customMap.Get(w)
+		if !ok {
+			t.Errorf("%s not found in map", w)
+			continue
+		}
+		if expectedCount != int(actualCount) {
+			t.Errorf("for word=%s, expected=%d, got=%d", w, expectedCount, actualCount)
+		}	
+	}
+}
