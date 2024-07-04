c8791976501da8ea149acba9eea4c6bd641167b8
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2024-07-04 13:36:38 +0530

    define common interface for hash maps

--- golang/hash_map_constants.go
+++ golang/hash_map_constants.go
@@ -2,3 +2,13 @@ package main
 
 const DefaultCapacity uint = 8
 const DefaultLoadFactor float64 = 0.5
+
+type HashMap[K string, V any] interface {
+	Put(key K, val V)
+	Get(key K) (V, bool)
+	GetOrDefault(key K, defaultVal V) (V)
+	String() string
+	getNumCompares() uint // Gives the number of equality comparisons used.
+						  // Is reset whenever the hash table is resized.
+	clearNumCompares() // Manually reset the counter for equality comparisons for profiling
+}
