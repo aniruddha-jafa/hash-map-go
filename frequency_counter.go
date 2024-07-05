f14d4d3db4f12a2c1fd679295fa63ca7991b6bb7
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2024-07-05 13:56:05 +0530

    take initial capacity & load as CL arguments

--- frequency_counter.go
+++ frequency_counter.go
@@ -11,16 +11,19 @@ import (
 // greater than or equal to the threshold,
 // and prints the result to  standard output
 func main() {
-	minLen := flag.Uint("n", 8, "Minimum length of words to consider")
-	mapImpl := flag.String("impl", "", "hash map implementation to use")
+	minLen := flag.Uint("n", 8, "minimum length of words to consider")
+	mapImpl := flag.String("impl", "", "hash map implementation to use - chain, linear")
+	initialCap := flag.Uint("cap", DefaultCapacity, "Initial capacity of hash map")
+	loadFactor := flag.Float64("load", DefaultLoadFactor, "Load factor of hash map")
+
 	flag.Parse()
 
 	var hashMap HashMap[string, uint]
 	switch *mapImpl {
 		case "chain":
-			hashMap = NewHashMapChaining[string, uint](DefaultCapacity, DefaultLoadFactor) 
+			hashMap = NewHashMapChaining[string, uint](*initialCap, *loadFactor) 
 		case "linear":
-			hashMap = NewHashMapLinearProbing[string, uint](DefaultCapacity, DefaultLoadFactor)
+			hashMap = NewHashMapLinearProbing[string, uint](*initialCap, *loadFactor)
 		default:
 			panic("Unknown impl: " + *mapImpl)
 	}
