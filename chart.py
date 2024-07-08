d6a5d00aaaf2e237835a36f24399aeb7f7b20b34
AS Jafa <27879504+aniruddha-jafa@users.noreply.github.com>
2024-07-08 14:05:25 +0530

    first draft for charts script

--- chart.py
+++ chart.py
@@ -2,15 +2,22 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 import seaborn as sns
 
+def create_equality_compares_graph(filepath: str):
+    df = pd.read_csv(filepath)
 
-df = pd.read_csv('./out/tale_linear.csv')
+    df1 = df[df.columns.difference(['key', 'cumulative_avg'])]
+    sns.scatterplot(data = df1, x = "s_no", y = "num_equality_compares", s=5, label="number of equality comparisons")
 
-df1 = df[df.columns.difference(['key', 'cumulative_avg'])]
-sns.scatterplot(data = df1, x = "s_no", y = "num_equality_compares", s=5, label="number of equality compares")
+    df2 = df[df.columns.difference(['key', 'num_equality_comparisons'])]
+    sns.scatterplot(data = df2, x = "s_no", y = "cumulative_avg", s=1, label="cumulative average of equality comparisons")
 
-df2 = df[df.columns.difference(['key', 'num_equality_compares'])]
-sns.scatterplot(data = df2, x = "s_no", y = "cumulative_avg", s=1, label="cumulative average of equality compares")
+    plt.legend()
+    plt.xlabel("Operation No.")
+    plt.ylabel("Number of equality comparisons")
 
-#concatenated = pd.concat([df1.assign(dataset='set1'), df2.assign(dataset='set2')])
-plt.legend()
-plt.show()
+    plt.show()
+
+
+if __name__=='__main__':
+    create_equality_compares_graph('./out/tale_linear.csv')
+    create_equality_compares_graph('./out/tale_chain.csv')
